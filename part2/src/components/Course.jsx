b0ef7388c256a7ea75ef369ec2b08e75a7cdf27f
Himanshu Pandey <38503747+krat0skal@users.noreply.github.com>
2024-02-25 22:57:16 +0530

    2.3*: Course information step8

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,5 +1,9 @@
 const Course = ({ course }) => {
     const parts = [...course.parts]
+    const exercises = parts.map(part => part.exercises)
+    const total = exercises.reduce((exercise,sumvalue) => exercise+sumvalue,0)
+    console.log('Exercises',exercises)
+    console.log('Total',total)
     return (
         <div>
             <h1>{course.name}</h1>
@@ -9,6 +13,7 @@ const Course = ({ course }) => {
                         {part.name} {part.exercises}
                     </p>
             )}
+            <b>Total of {total} exercises</b>
         </div>
     )
 }
