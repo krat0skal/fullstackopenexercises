bd7e7191e95a7547e4f789bc3826744cbc53d6f6
Himanshu Pandey <38503747+krat0skal@users.noreply.github.com>
2024-02-26 00:15:32 +0530

    2.5: Separate module step10

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,19 +1,42 @@
-const Course = ({ course }) => {
-    const parts = [...course.parts]
-    const exercises = parts.map(part => part.exercises)
-    const total = exercises.reduce((exercise,sumvalue) => exercise+sumvalue,0)
-    console.log('Exercises',exercises)
-    console.log('Total',total)
+const Header = ({ props }) => {
     return (
-        <div>
-            <h1>{course.name}</h1>
-            {parts.map(
-                part =>
-                    <p key={part.id}>
-                        {part.name} {part.exercises}
+        <>
+            <h1 key={props.id}>
+                {props.name}
+            </h1>
+            <Parts props={props.parts} />
+        </>
+    )
+
+}
+
+const Parts = ({ props }) => {
+    const exercises = props.map(prop => prop.exercises)
+    const total = exercises.reduce((exercise,sumValue) => exercise+sumValue,0)
+    console.log("parts array",props)
+    console.log("exercises array",exercises)
+    console.log("total",total)
+    return (
+        <>
+            {props.map(
+                prop =>
+                    <p key={prop.id}>
+                        {prop.name} {prop.exercises}
                     </p>
             )}
             <b>Total of {total} exercises</b>
+        </>
+    )
+}
+
+const Course = ({ courses }) => {
+    const coursesArr = [...courses]
+    console.log("courses ", coursesArr)
+    return (
+        <div>
+            {coursesArr.map(course =>
+                <Header key={course.id} props={course} />
+            )}
         </div>
     )
 }
