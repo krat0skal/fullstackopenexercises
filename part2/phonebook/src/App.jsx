00dc368a121fa20b9e5bde43f5fa105f869814a5
Himanshu Pandey <38503747+krat0skal@users.noreply.github.com>
2024-02-28 22:03:53 +0530

    2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone:'903-350-610' }
   ])
-
-  const checkDuplicate =() => {
-
-  }
   
   const addPerson = (event) => {
     event.preventDefault()
-    const newPerson ={ name: newName }
-    const boolArr = persons.map(person => JSON.stringify(person) === JSON.stringify(newPerson))
+    const newPerson ={ name: newName, phone: newPhone }
+    const boolArr = persons.map(person => JSON.stringify(person.name) === JSON.stringify(newPerson.name))
     console.log('boolarr is',boolArr)
     if(boolArr.includes(true)){
       alert(`${newName} is already added to phonebook`)
@@ -23,10 +19,16 @@ const App = () => {
     }
   }
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handlePhoneChange = (event) => {
+    console.log(event.target.value)
+    setNewPhone(event.target.value)
   }
 
   return (
@@ -37,11 +39,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newPhone} onChange={handlePhoneChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => <div key={person.name}>{person.name}</div>)}
+        {persons.map(person => <div key={person.name}>{person.name} {person.phone}</div>)}
     </div>
   )
 }
