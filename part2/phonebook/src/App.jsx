399294f65a2f09859f7dbe98154820a72b9744ae
Himanshu Pandey <38503747+krat0skal@users.noreply.github.com>
2024-03-01 23:24:00 +0530

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,22 +4,12 @@ const App = () => {
     { name: 'Arto Hellas', phone:'903-350-610' }
   ])
   
-  const addPerson = (event) => {
-    event.preventDefault()
-    const newPerson ={ name: newName, phone: newPhone }
-    const boolArr = persons.map(person => JSON.stringify(person.name) === JSON.stringify(newPerson.name))
-    console.log('boolarr is',boolArr)
-    if(boolArr.includes(true)){
-      alert(`${newName} is already added to phonebook`)
-    } else{
-      setPersons(persons.concat(newPerson))
-      setNewName('')
-      console.log('New Name :',newName)
-      console.log('Person array:',persons)
-    }
-  }
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filterName, setfilterName] = useState('')
+  const dspPersons = filterName === ''
+                ? persons
+                : persons.filter(person => person.name.toLowerCase().includes(filterName.toLowerCase()))
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -31,9 +21,37 @@ const App = () => {
     setNewPhone(event.target.value)
   }
 
+  const handlefilterNameChange = (event) => {
+    console.log(event.target.value)
+    setfilterName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    console.log('New Name :',newName)
+    event.preventDefault()
+    const newPerson ={ name: newName, phone: newPhone }
+    const boolArr = persons.map(person => JSON.stringify(person.name) === JSON.stringify(newPerson.name))
+    console.log('boolarr is',boolArr)
+    if (newName === ''){
+      alert(`Name cannot be empty`)
+    }else if(newPhone === ''){
+      alert(`Phone cannot be empty`)
+    }else if(boolArr.includes(true)){
+      alert(`${newName} is already added to phonebook`)
+    } else{
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+      setNewPhone('')
+      console.log('New Name :',newName)
+      console.log('Person array:',persons)
+    }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={filterName} onChange={handlefilterNameChange}/></div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -46,7 +64,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => <div key={person.name}>{person.name} {person.phone}</div>)}
+        {dspPersons.map(person => <div key={person.name}>{person.name} {person.phone}</div>)}
     </div>
   )
 }
