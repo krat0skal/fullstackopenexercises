634bc5e4b0cece48cf90b0149c197fb8a5e1da90
Himanshu Pandey <38503747+krat0skal@users.noreply.github.com>
2024-04-09 10:06:42 +0700

    3.6: Phonebook backend step 6

--- part3/index.js
+++ part3/index.js
@@ -3,40 +3,41 @@ first we initialize node repowith node init
 install express dependency with npm install express
 install nodemon dev dependency with npm install --save-dev nodemon
 declare dev script in package.json
-
+to read request.body, please use app.use(express.json())
 */
 
 
 const express = require('express')
 const app = express()
+app.use(express.json())
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
-app.get('/api/persons',(request, response) =>{
+app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-app.get('/info',(request, response) =>{
+app.get('/info', (request, response) => {
     let currDate = new Date()
     const numOfPerson = persons.length
     console.log(`Phonebook has info for ${numOfPerson} people`)
@@ -46,10 +47,10 @@ app.get('/info',(request, response) =>{
     // response.json(persons)
 })
 
-app.get('/api/persons/:id',(request, response) => {
+app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
-    if (person){
+    if (person) {
         response.json(person)
     } else {
         console.log('No erson found')
@@ -57,16 +58,49 @@ app.get('/api/persons/:id',(request, response) => {
     }
 })
 
-app.delete('/api/persons/:id',(request,response) => {
+app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
-    if (persons.find(person => person.id === id)){
+    if (persons.find(person => person.id === id)) {
         persons = persons.filter(person => person.id !== id)
         response.sendStatus(204)
-    } else{
+    } else {
         response.sendStatus(404)
     }
 })
 
+const generateId = () => {
+    return (Math.floor(Math.random() * 100))
+}
+
+app.post('/api/persons', (request, response) => {
+    if (!request.body) {
+        response.status(400).json({
+            error: 'Body Not Found'
+        })
+    } else if (!request.body.name) {
+        response.status(400).json({
+            error: 'Name Not Found'
+        })
+    } else if (!request.body.number) {
+        response.status(400).json({
+            error: 'Number Not Found'
+        })
+    } else if (persons.find(person => person.name === request.body.name)) {
+        response.status(400).json({
+            error: 'Person already exists'
+        })
+    } else {
+        console.log(request.body)
+        const person = {
+            id: generateId(),
+            name: request.body.name,
+            number: request.body.number
+        }
+        persons = persons.concat(person)
+        console.log(persons)
+        response.json(person)
+    }
+})
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
