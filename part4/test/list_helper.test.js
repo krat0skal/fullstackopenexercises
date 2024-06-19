204278948a7cca511ca57a95956be7adbec29066
Himanshu Pandey <38503747+krat0skal@users.noreply.github.com>
2024-06-19 21:37:39 +0530

    4.5*: Helper Functions and Unit Tests, step 3 : added function to check favouriteblog and tested it

--- part4/test/list_helper.test.js
+++ part4/test/list_helper.test.js
@@ -65,3 +65,88 @@ describe('total likes', () => {
         assert.strictEqual(result, 116)
     })
 })
+
+describe('Favourite Blog', () => {
+    const emptyList = []
+
+    test('when list is empty', () => {
+        const result = listHelper.favoriteBlog(emptyList)
+        assert.deepStrictEqual(result, {
+            title: 'NA',
+            author: 'NA',
+            likes: 0
+        })
+    })
+
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('when list has only one blog, it is same as favouriteblog', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        assert.deepStrictEqual(result, {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            likes: 5
+        })
+    })
+
+    const listWithManyBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 16,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'The Bible',
+            author: 'Unknown',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 8,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Rap 101',
+            author: 'Kendrick Lamar',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 210,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'how to become washed',
+            author: 'Kanye West',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'how to be cringe',
+            author: 'Eminem',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 100,
+            __v: 0
+        }
+    ]
+
+    test('when list has many blogs, fave blog is the one with most likes', () => {
+        const result = listHelper.favoriteBlog(listWithManyBlog)
+        assert.deepStrictEqual(result, {
+            title: 'Rap 101',
+            author: 'Kendrick Lamar',
+            likes: 210
+        })
+    })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -8,9 +8,29 @@ const totalLikes = (blogs) => {
     }
     return blogs.length === 0
         ? 0
-        : blogs.map(blog =>blog.likes).reduce(reducer,0)
+        : blogs.map(blog => blog.likes).reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return {
+            title: 'NA',
+            author: 'NA',
+            likes: 0
+        }
+    } else {
+        const maxLikes = blogs.map(blog => blog.likes).toSorted((a, b) => b - a)[0]
+        const indexMax = blogs.map(blog => blog.likes).indexOf(maxLikes)
+        const returnObj = {
+            title: blogs[indexMax].title,
+            author: blogs[indexMax].author,
+            likes: blogs[indexMax].likes
+        }
+        return returnObj
+    }
 }
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
