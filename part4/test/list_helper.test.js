34fa5a0322f5db80cb742221c80df5759e63a2f9
Himanshu Pandey <38503747+krat0skal@users.noreply.github.com>
2024-06-19 17:52:38 +0530

    4.4: Helper Functions and Unit Tests, step 2 : added a total function and it tested successfully

--- part4/test/list_helper.test.js
+++ part4/test/list_helper.test.js
@@ -3,8 +3,65 @@ const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
-  const blogs = []
+    const blogs = []
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+    const emptyList = []
+
+    test('when list is empty', () => {
+        const result = listHelper.totalLikes(emptyList)
+        assert.strictEqual(result, 0)
+    })
+
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 5)
+    })
+
+    const listWithManyBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 90,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 21,
+            __v: 0
+        }
+    ]
+
+    test('when list has many blogs, total is correct', () => {
+        const result = listHelper.totalLikes(listWithManyBlog)
+        assert.strictEqual(result, 116)
+    })
 })
