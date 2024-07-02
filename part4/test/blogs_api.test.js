d62554582d39b42adde0e664b4e46f11e74de950
Himanshu Pandey <38503747+krat0skal@users.noreply.github.com>
2024-07-02 21:31:18 +0530

    4.9: Blog List Tests, step 2 : unique identifierr is id insteead of _id

--- part4/test/blogs_api.test.js
+++ part4/test/blogs_api.test.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const assert = require('assert')
-const Blog =require('../models/blog')
+const Blog = require('../models/blog')
 const helper = require('./test_helper')
+var lodasher = require('lodash');
+
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -26,7 +28,14 @@ test('blogs are returned as json', async () => {
 test('number of blogs are 2', async () => {
   const response = await api.get('/api/blogs')
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
-  })
+})
+test('unique identifier of blogs is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const keys = lodasher.keys(response.body[0])
+  console.log(keys)
+  console.log(keys.filter(key => key === 'id'))
+  assert.strictEqual(keys.filter(key => key === 'id')[0], 'id')
+})
 
 after(async () => {
   await mongoose.connection.close()
